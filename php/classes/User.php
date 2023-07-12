8db2255327d6e92dbbfcf67103138c8ed508cdd2
smannasm <135710215+smannasm@users.noreply.github.com>
2023-07-12 09:22:52 +0300

    Add files via upload

---
+++ php/classes/User.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+class User {
+  private $name;
+  private $lastname;
+  private $email;
+  private $id;
+
+  function __construct($id, $name, $lastname, $email)
+  {
+    $this->id = $id;
+    $this->name = $name;
+    $this->lastname = $lastname;
+    $this->email = $email;
+  }
+
+  function getId() {return $this->id;}
+  function getName() {return $this->name;}
+  function getLastname() {return $this->lastname;}
+  function getEmail() {return $this->email;}
+
+  //Статический метод добавления (регистрации) пользователя
+  static function addUser($name, $lastname, $email, $pass) {
+    global $mysqli;
+
+    $email = mb_strtolower(trim($email));
+    $pass = trim($pass);
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+
+    $result = $mysqli->query("SELECT * FROM `user` WHERE `email`='$email'");
+
+    if ($result->num_rows != 0) {
+      return json_encode(["result"=>"exist"]);
+    } else {
+      $mysqli->query("INSERT INTO `user`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
+      return json_encode(["result"=>"success"]);
+    }
+
+  }
+  //Статический метод авторизации пользователя
+  static function authUser($email, $pass) {
+    global $mysqli;
+    //return "User auth";
+    $email = mb_strtolower($_POST["email"]);
+    $pass = trim($_POST["pass"]);
+    //$pass = password_hash($pass, PASSWORD_DEFAULT);
+    $result = $mysqli->query("SELECT * FROM `user` WHERE `email`='$email'");
+//var_dump($result);
+$result = $result->fetch_assoc();
+$pass_hash = $result["pass"];
+
+if (password_verify($pass, $pass_hash)) {
+  
+  $_SESSION["id"] = $result["id"];
+  $_SESSION["name"] = $result["name"];
+  $_SESSION["lastname"] = $result["lastname"];
+  $_SESSION["email"] = $result["email"];
+  echo $result["id"];
+      return json_encode(["result"=>"exist"]);
+    } else {
+      return json_encode(["result"=>"success"]);
+    }
+  }
+}
+// if ($result->num_rows != 0) {
+     
+//       return json_encode(["result"=>"exist"]);
+//     } else {
+//       return json_encode(["result"=>"success"]);
+//     }
+
+//   }
